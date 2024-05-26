b8838b2ed41bb8df69182cd9032dc28e9d792c11
Mykola Mazniuk <mykola.mazniuk@gmail.com>
2024-05-26 22:35:59 +0200

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
-import { UserSchema } from "./models/user.js";
+import UserModel from "./models/user.js";
 
 dotenv.config();
 
 // console.log(process.env.MONGODB_CONNECT)
 mongoose
-  .connect(process.env.MONGODB_CONNECT)
+  .connect(process.env.MONGODB_BLOG_CONNECT)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
@@ -18,23 +19,34 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: passwordHash,
+    });
+
+    const user = await doc.save()
+
     res.json({
-        success: true,
-        message: "Register success",
+        user
     })
 });
 
-
-// app.get("/", (req, res) => {
-//     res.send("Hello World");
-//     console.log("Hello World from console");
-// });
+app.get("/", (req, res) => {
+    res.send("Hello World");
+    console.log("Hello World from console");
+});
 
 // app.post("/auth/login", (req, res) => {
 

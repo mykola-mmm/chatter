c9171b01d94ad67d1e2c8dffbd94433ce874d6b8
Mykola Mazniuk <mykola.mazniuk@gmail.com>
2024-05-29 15:17:26 +0200

    added file upload, install multer, small refactoring

--- server/index.js
+++ server/index.js
@@ -1,47 +1,63 @@
-import express from "express";
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs-extra';
 
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import { registerValidation, loginValidation, postCreateValidation } from "./validations/validations.js";
-import checkAuth from "./utils/checkAuth.js";
-
-import * as UserController from "./controllers/userController.js";
-import * as PostController from "./controllers/postController.js";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { registerValidation, loginValidation, postCreateValidation } from './validations/validations.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
 
 dotenv.config();
 
 // console.log(process.env.MONGODB_CONNECT)
 mongoose
   .connect(process.env.MONGODB_BLOG_CONNECT)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    fs.mkdirsSync('uploads');
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+})
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
-app.post("/auth/login", loginValidation, UserController.login);
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post("/auth/register", registerValidation, UserController.register);
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  console.log(req.file.path)
+  res.json({
+    url: `uploads/${req.file.originalname}`
+  })
+});
 
-app.get("/auth/me", checkAuth, UserController.getMe);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
+app.delete('/posts/:id', checkAuth, PostController.remove);
 
-app.get("/posts", PostController.getAll);
-app.get("/posts/:id", PostController.getOne);
-app.post("/posts", checkAuth, postCreateValidation, PostController.create);
-app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", checkAuth, PostController.update);
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World");
-//     console.log("Hello World from console");
-// });
+const port = process.env.PORT || 3000;
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
 
-  console.log("Server OK");
+  console.log(`Server running on ${port}`);
 });
 
